69a9695f4d8bb5b858e928fc9597dc25670d64c4
Ankit Bisht <ankitbisht9837@gmail.com>
2025-01-23 21:55:01 +0000

    final commi

--- firbase.ts
+++ firbase.ts
@@ -7,14 +7,14 @@ import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQFAri_itV7bHmZcbPT0Ht90EP2LawPVI",
-  authDomain: "portfoliyo-c48b6.firebaseapp.com",
-  databaseURL: "https://portfoliyo-c48b6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "portfoliyo-c48b6",
-  storageBucket: "portfoliyo-c48b6.firebasestorage.app",
-  messagingSenderId: "662344714053",
-  appId: "1:662344714053:web:ba4c8a40f14b7a879ae02c",
-  measurementId: "G-7W6PDEK7G3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTD
 };
 
 // Initialize Firebase
